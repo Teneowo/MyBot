696c07df0eccfd5d91c7a1b7504db51f62cb6811
Oluwasanjo Okeowo <teniokeowo@gmail.com>
2022-02-25 15:27:35 -0500

    Token hidden

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const config = require('./config.json');
 const Discord = require('discord.js');
 
-//const client = new Discord.Client();
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 });
@@ -14,7 +13,5 @@ client.once('ready', () => {
 
 
 
-console.log(config.token);
 
 client.login(config.token);
-//client.login('OTQ2ODM5MTIyMzY5MjAwMTY4.Yhkipg.IKbi9b-7iqv1Nj3TBRggkaqu8SY');
