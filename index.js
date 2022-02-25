6804602623702b32e4639fbb25b8cd86c4d58565
Oluwasanjo Okeowo <teniokeowo@gmail.com>
2022-02-25 13:59:20 -0500

    Basic functions added for bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Vestige is online!');
+});
+
+
+
+
+
+
+client.login('OTQ2ODM5MTIyMzY5MjAwMTY4.Yhkipg.bKq-XXCLoGMsfIuaJW_smuhdLOs');
